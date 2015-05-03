c7ea675d178cd4e91f6507bfdec90310eb6ccf36
Thorsten Lorenz <thlorenz@gmx.de>
2015-05-03 15:58:20 -0400

    most of mov working
    - need to adapt esp,ebp and eip

---
+++ test/fixtures/mov_reg_imd.asm
@@ -0,0 +1,19 @@
+; vim: ft=nasm
+
+section .text
+global _start
+_start:
+  nop
+.gai_s:
+  mov eax, 0x1
+  mov ecx, 0x2
+  mov edx, 0x3
+  mov ebx, 0x4
+  mov esp, 0x5
+  mov ebp, 0x6
+  mov esi, 0x7
+  mov edi, 0x8
+.gai_e:
+  mov eax,1
+  mov ebx,0
+  int 80H
