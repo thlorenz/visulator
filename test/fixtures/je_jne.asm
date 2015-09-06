8358592d662595f90ae3bc1f59944762ca646833
Thorsten Lorenz <thlorenz@gmx.de>
2015-09-06 22:15:44 +1000

    improving memory logging and traced issue to gai

--- test/fixtures/je_jne.asm
+++ test/fixtures/je_jne.asm
@@ -16,18 +16,18 @@ _start:
   mov  ecx, 0x6   ; shouldn't get here
 
 .zero:
-  mov  ecx, 0x0
-  test ecx, ecx
-  jne .not_zero
+   mov  ecx, 0x0
+  ;test ecx, ecx
+  ;jne .not_zero
 
-  mov  ecx, 0x1   ; should get here
-  test ecx, ecx
-  jne .not_zero
+  ;mov  ecx, 0x1   ; should get here
+  ;test ecx, ecx
+  ;jne .not_zero
 
-  mov  ecx, 0x66   ; shouldn't get here
+  ;mov  ecx, 0x66   ; shouldn't get here
 
-.not_zero:
-  mov  ecx, 0x0
+;.not_zero:
+  ;mov  ecx, 0x0
 
 .gai_e:
   mov  eax,1
